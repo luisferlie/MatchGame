9b5afa10bb3de551b44dd8e03e952f59b7709316
luisferlie <luisferlie@gmail.com>
2020-10-30 08:40:04 +0100

    first commit

---
+++ hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hello
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            OperatorExample(); ;
+        }
+
+        private static void OperatorExample()
+        {
+            int withd = 3;
+            int puta = 4;
+        }
+    }
+}
